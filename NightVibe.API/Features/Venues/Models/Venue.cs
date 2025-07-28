688be86ffd2eacd38f4ea6ddeaab8a77e715ccce
Alan Perez <alan@Mac.attlocal.net>
2025-07-27 23:14:36 -0500

    setup Venue Model

--- NightVibe.API/Features/Venues/Models/Venue.cs
+++ NightVibe.API/Features/Venues/Models/Venue.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using NightVibe.API.Features.Events.Models;
 
-namespace NightVibe.API.Features.Events.Models;
+namespace NightVibe.API.Features.Venues.Models;
 
 public class Venue
 {
