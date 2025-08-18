bc92f24ddf733baf62c7291dca96f386394d504d
Alan Perez <alan@Alans-MacBook-Pro.local>
2025-08-18 16:03:09 -0500

    migrations

--- NightVibe.API/Migrations/20250809232349_AddNeighborhooodsAndVenueFK.Designer.cs
+++ NightVibe.API/Migrations/20250809232349_AddNeighborhooodsAndVenueFK.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NightVibe.API.Data;
 
@@ -11,9 +12,11 @@ using NightVibe.API.Data;
 namespace NightVibe.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250809232349_AddNeighborhooodsAndVenueFK")]
+    partial class AddNeighborhooodsAndVenueFK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NightVibe.API/Migrations/20250809232349_AddNeighborhooodsAndVenueFK.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NightVibe.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNeighborhooodsAndVenueFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "NeighborhoodId",
+                table: "Venues",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Neighborhoods",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Slug = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Neighborhoods", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Venues_NeighborhoodId",
+                table: "Venues",
+                column: "NeighborhoodId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Venues_Neighborhoods_NeighborhoodId",
+                table: "Venues",
+                column: "NeighborhoodId",
+                principalTable: "Neighborhoods",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Venues_Neighborhoods_NeighborhoodId",
+                table: "Venues");
+
+            migrationBuilder.DropTable(
+                name: "Neighborhoods");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Venues_NeighborhoodId",
+                table: "Venues");
+
+            migrationBuilder.DropColumn(
+                name: "NeighborhoodId",
+                table: "Venues");
+        }
+    }
+}
